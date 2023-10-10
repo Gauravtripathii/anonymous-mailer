7651fdd834f5a731e47ee3c1b5a014306ea8697d
gauravtripathii <gauravtripathii>
2023-10-11 00:30:51 +0530

    basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,22 @@
 
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <h1>Anonymous Mailer</h1>
+      <div className="form-container">
+        <p>
+          <input type="text" placeholder="Recipient's Email" />
+        </p>
+        <p>
+          <input type="text" placeholder="Subject of your email" />
+        </p>
+        <p>
+          <textarea></textarea>
+        </p>
+        <p>
+          <button>Send</button>
+        </p>
+      </div>
+    </main>
   )
 }
