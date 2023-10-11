d44d023f71408845f389c02026791af2648179c7
gauravtripathii <gauravtripathii>
2023-10-11 18:05:45 +0530

    font family updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,30 @@
-import './globals.css';
+import "./globals.css";
 import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}<Toaster /></body>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Martian+Mono&display=swap"
+          rel="stylesheet"
+        />
+        <title>Send Emails Anonymously</title>
+      </head>
+      <body>
+        {children}
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
